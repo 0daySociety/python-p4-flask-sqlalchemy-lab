b1de5ac2b4e561819ef22e8c7573ebb5c900faa1
kimemia21 <bobbymbogo71@gmail.com>
2023-09-24 17:06:20 +0300

    Done

--- server/app.py
+++ server/app.py
@@ -19,15 +19,82 @@ def home():
 
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
-    return ''
-
+    animal = Animal.query.filter(Animal.id == id).first()
+    
+    if not animal:
+        response_body = "<h1>404 pet not found</h1>"
+        response = make_response(response_body, 404)
+        return response
+    
+    zookeeper = Zookeeper.query.filter_by(id=animal.zookeeper_id).first()
+    my_enclosure=Enclosure.query.filter_by(id=animal.enclosure_id).first()
+    
+    response_body = f'''
+        <ul>
+            <li>ID: {animal.id}</li>
+            <li>Name: {animal.name}</li>
+            <li>Species: {animal.species}</li>
+            <li>Zookeeper: {zookeeper.name if zookeeper else 'Unknown'}</li>
+            <li>Enclosure:{my_enclosure.environment}</li>
+        </ul>'''
+    
+    response = make_response(response_body, 200)
+    return response
+    
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
-    return ''
+    def animal():
+        animals = Animal.query.filter(Animal.zookeeper_id == id).all()
+        
+        animal_list = []
+        for animal in animals:
+            animal_list.append(f"<li>Animal : {animal.name}</li>")
+        return "\n".join(animal_list) 
+    zookeeper = Zookeeper.query.filter(Zookeeper.id == id).first()
+   
+    if not zookeeper:
+        response = "<h1>Error 404 page not found.<h1>"
+        return make_response(response, 404)
 
+    animal_names = animal()  
+
+    response = f'''
+        <h1>Zookeeper Details</h1>
+        <ul>
+            <li>ID: {zookeeper.id}</li>
+            <li>Name: {zookeeper.name}</li>
+            <li>Birthday: {zookeeper.birthday}</li>
+            Animal :{animal_names}
+        </ul>
+    '''
+
+    return make_response(response, 200)
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
-    return ''
+    enclosure=Enclosure.query.filter(Enclosure.id==id).first()
+ 
+    def animal():
+        animals=Animal.query.filter(Animal.enclosure_id==id).all()
+        animal_list=[]
+       
+        for animal in animals:
+            print(animal.name)
+            animal_list.append(f"<li>Animal :{animal.name}</li>")
+        return "\n".join(animal_list)        
+
+
+        
+
+
+    enclosure_animal_name=animal()
+    response =f'''
+                <li>ID:{enclosure.id}</li>
+                <li>Enviroment:{enclosure.environment}</li>
+                <li>Open to Visitors :{enclosure.open_to_visitors}</li>
+                {enclosure_animal_name}
+                 '''
+    
+    return make_response(response,200)
 
 
 if __name__ == '__main__':
--- server/models.py
+++ server/models.py
@@ -1,3 +1,4 @@
+import datetime
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 
@@ -5,19 +6,31 @@ metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
 })
 
-db = SQLAlchemy(metadata=metadata)
-
-class Zookeeper(db.Model):
-    __tablename__ = 'zookeepers'
-
-    id = db.Column(db.Integer, primary_key=True)
-
-class Enclosure(db.Model):
-    __tablename__ = 'enclosures'
-
-    id = db.Column(db.Integer, primary_key=True)
+try:
+    db = SQLAlchemy(metadata=metadata)
+except Exception as e:
+    print("got this error", str(e))    
 
 class Animal(db.Model):
-    __tablename__ = 'animals'
-
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    species = db.Column(db.String(100), nullable=False)
+    zookeeper_id = db.Column(db.Integer, db.ForeignKey('zookeeper.id'))
+    enclosure_id = db.Column(db.Integer, db.ForeignKey('enclosure.id'))
+
+class Zookeeper(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    birthday = db.Column(db.Date)
+    animals = db.relationship('Animal', backref='zookeeper', lazy=True)
+
+class Enclosure(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    environment = db.Column(db.String(100), nullable=False)
+    open_to_visitors = db.Column(db.Boolean, default=False)
+    animals = db.relationship('Animal', backref='enclosure', lazy=True)
+
+
+
+
+
